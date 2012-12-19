9aca8771c56f44c9cb03c0357d1f860164d13548
Andrew Petersen <kirbysayshi@gmail.com>
2012-12-18 23:51:03 -0500

    better AMD guard for runtime and vash.exports. #18

--- src/vash.exports.js
+++ src/vash.exports.js
@@ -6,7 +6,7 @@
 	// https://github.com/RobertWHurst/LucidJS/blob/master/lucid.js
 
 	if(typeof define === 'function' && define['amd']){
-		define(vash); // AMD
+		define(function(){ return vash }); // AMD
 	} else if(typeof module === 'object' && module['exports']){
 		module['exports'] = vash; // NODEJS
 	} else {
