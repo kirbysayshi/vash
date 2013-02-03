df3c6c183013cc13afde1fbcbc71bb257b752002
Andrew Petersen <kirbysayshi@gmail.com>
2013-02-03 01:50:46 -0500

    refactor vash.build, vash.compileHelper
    the logic is practically the same.

--- src/vash.exports.js
+++ src/vash.exports.js
@@ -64,48 +64,34 @@
 	};
 
 	///////////////////////////////////////////////////////////////////////////
-	// VASH.BATCH
-	//
-	// Allow multiple templates to be contained within the same string.
-	// Templates are separated via a sourceURL-esque string:
-	//
-	// //@batch = tplname/or/path
-	//
-	// The separator is forgiving in terms of whitespace:
-	//
-	// // @      batch=tplname/or/path
-	//
-	// Is just as valid.
-	//
-	// Returns the compiled templates as named properties of an object.
+	// HELPER AND BATCH COMPILATION
 
-	exports['batch'] = function(markup, options){
+	var  slice = Array.prototype.slice
 
-		var  separator = /^\/\/\s*@\s*batch\s*=\s*(.*?)$/
-			,tpls = splitByNamedTpl(separator, markup, function(ma, name){
-				return name.replace(/^\s+|\s+$/, '');
-			}, true);
+		,reHelperFuncHead = /vash\.helpers\.([^= ]+?)\s*=\s*function([^(]*?)\(([^)]*?)\)\s*{/
+		,reHelperFuncTail = /\}$/
 
-		if(tpls){
-			Object.keys(tpls).forEach(function(path){
-				tpls[path] = vash.compile('@{' + tpls[path] + '}', options);
-			});
+		,reBatchSeparator = /^\/\/\s*@\s*batch\s*=\s*(.*?)$/
 
-			tpls.toClientString = function(){
-				return Object.keys(tpls).reduce(function(prev, curr){
-					if(curr === 'toClientString'){
-						return prev;
-					}
-					return prev + tpls[curr].toClientString() + '\n';
-				}, '')
-			}
-		}
-
-		return tpls;
-	}
-
-	// do the actual work of splitting the string via the batch separator
-	var splitByNamedTpl = function(reSeparator, markup, resultHandler, keepMatch){
+	// Given a separator regex and a function to transform the regex result
+	// into a name, take a string, split it, and group the rejoined strings
+	// into an object.
+	// This is useful for taking a string, such as
+	//
+	// 		// tpl1
+	// 		what what
+	// 		and more
+	//
+	// 		// tpl2
+	// 		what what again
+	//
+	// and returning:
+	//
+	//		{
+	//			tpl1: 'what what\nand more\n',
+	//			tpl2: 'what what again'
+	//		}
+	var splitByNamedTpl = function(reSeparator, markup, resultHandler){
 
 		var  lines = markup.split(/[\n\r]/g)
 			,tpls = {}
@@ -122,9 +108,7 @@
 				tpls[currentPath] = [];
 			}
 
-			if(!handlerResult || keepMatch){
-				tpls[currentPath].push(line);
-			}
+			tpls[currentPath].push(line);
 		});
 
 		Object.keys(tpls).forEach(function(key){
@@ -134,50 +118,26 @@
 		return tpls;
 	}
 
-	// VASH.BATCH
-	///////////////////////////////////////////////////////////////////////////
+	// The logic for compiling a giant batch of templates or several
+	// helpers is nearly exactly the same. The only difference is the
+	// actual compilation method called, and the regular expression that
+	// determines how the giant string is split into named, uncompiled
+	// template strings.
+	var compileBatchOrHelper = function(type, str, options){
 
-	///////////////////////////////////////////////////////////////////////////
-	// HELPER INSTALLATION
+		var separator = type === 'helper'
+			? reHelperFuncHead
+			: reBatchSeparator;
 
-	var  slice = Array.prototype.slice
-		,reFuncHead = /vash\.helpers\.([^= ]+?)\s*=\s*function([^(]*?)\(([^)]*?)\)\s*{/
-		,reFuncTail = /\}$/
-
-	var compileSingleHelper = function(str, options){
-
-		options = options || {};
-
-			// replace leading/trailing spaces, and parse the function head
-		var  def = str.replace(/^[\s\n\r]+|[\s\n\r]+$/, '').match(reFuncHead)
-			// split the function arguments, kill all whitespace
-			,args = def[3].split(',').map(function(arg){ return arg.replace(' ', '') })
-			,name = def[1]
-			,body = str
-				.replace( reFuncHead, '' )
-				.replace( reFuncTail, '' )
-
-		// Wrap body in @{} to simulate it actually being inside a function
-		// definition, since we manually stripped it. Without this, statements
-		// such as `this.what = "what";` that are at the beginning of the body
-		// will be interpreted as markup.
-		body = '@{' + body + '}';
-
-		// `args` and `asHelper` inform `vash.compile/link` that this is a helper
-		options.args = args;
-		options.asHelper = name;
-		return vash.compile(body, options);
-	}
-
-	exports['compileHelper'] = function reg(str, options){
-
-		var tpls = splitByNamedTpl(reFuncHead, str, function(ma, name){
+		var tpls = splitByNamedTpl(separator, str, function(ma, name){
 			return name.replace(/^\s+|\s+$/, '');
-		}, true);
+		});
 
 		if(tpls){
 			Object.keys(tpls).forEach(function(path){
-				tpls[path] = compileSingleHelper(tpls[path], options);
+				tpls[path] = type === 'helper'
+					? compileSingleHelper(tpls[path], options)
+					: vash.compile('@{' + tpls[path] + '}', options);
 			});
 
 			tpls.toClientString = function(){
@@ -193,7 +153,72 @@
 		return tpls;
 	}
 
-	// HELPER INSTALLATION
+	var compileSingleHelper = function(str, options){
+
+		options = options || {};
+
+			// replace leading/trailing spaces, and parse the function head
+		var  def = str.replace(/^[\s\n\r]+|[\s\n\r]+$/, '').match(reHelperFuncHead)
+			// split the function arguments, kill all whitespace
+			,args = def[3].split(',').map(function(arg){ return arg.replace(' ', '') })
+			,name = def[1]
+			,body = str
+				.replace( reHelperFuncHead, '' )
+				.replace( reHelperFuncTail, '' )
+
+		// Wrap body in @{} to simulate it actually being inside a function
+		// definition, since we manually stripped it. Without this, statements
+		// such as `this.what = "what";` that are at the beginning of the body
+		// will be interpreted as markup.
+		body = '@{' + body + '}';
+
+		// `args` and `asHelper` inform `vash.compile/link` that this is a helper
+		options.args = args;
+		options.asHelper = name;
+		return vash.compile(body, options);
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+	// VASH.COMPILEHELPER
+	//
+	// Allow multiple helpers to be compiled as templates, for helpers that
+	// do a lot of markup output.
+	//
+	// Takes a template such as:
+	//
+	// 		vash.helpers.p = function(text){
+	// 			<p>@text</p>
+	// 		}
+	//
+	// And compiles it. The template is then added to `vash.helpers`.
+	//
+	// Returns the compiled templates as named properties of an object.
+	//
+	// This is string manipulation at its... something. It grabs the arguments
+	// and function name using a regex, not actual parsing. Definitely error-
+	// prone, but good enough. This is meant to facilitate helpers with complex
+	// markup, but if something more advanced needs to happen, a plain helper
+	// can be defined and markup added using the manual Buffer API.
+	exports['compileHelper'] = compileBatchOrHelper.bind(null, 'helper');
+
+	///////////////////////////////////////////////////////////////////////////
+	// VASH.BATCH
+	//
+	// Allow multiple templates to be contained within the same string.
+	// Templates are separated via a sourceURL-esque string:
+	//
+	// //@batch = tplname/or/path
+	//
+	// The separator is forgiving in terms of whitespace:
+	//
+	// // @      batch=tplname/or/path
+	//
+	// Is just as valid.
+	//
+	// Returns the compiled templates as named properties of an object.
+	exports['batch'] = compileBatchOrHelper.bind(null, 'batch');
+
+	// HELPER AND BATCH COMPILATION
 	///////////////////////////////////////////////////////////////////////////
 
 	/************** Begin injected code from build script */
