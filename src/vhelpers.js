66c722633a5ce4c5e409805bcce7e5a74f5ce10e
Andrew Petersen <kirbysayshi@gmail.com>
2013-02-09 03:57:38 -0500

    vash.helpers.trim in optional helpers

--- src/vhelpers.js
+++ src/vhelpers.js
@@ -3,6 +3,11 @@
 
 	var helpers = vash.helpers;
 
+	// Trim whitespace from the start and end of a string
+	helpers.trim = function(val){
+		return val.replace(/^\s*|\s*$/g, '');
+	}
+
 	///////////////////////////////////////////////////////////////////////////
 	// EXAMPLE HELPER: syntax highlighting
 
