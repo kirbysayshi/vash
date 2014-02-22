bb3eff8acb79ad29b221310a8f38773e4ac641f8
Andrew Petersen <kirbysayshi@gmail.com>
2014-02-21 21:42:50 -0500

    markupattribute does not need a name

--- experiments/stated/nodes/markupattribute.js
+++ experiments/stated/nodes/markupattribute.js
@@ -1,6 +1,5 @@
 module.exports = function MarkupAttributeNode() {
   this.type = 'VashMarkupAttribute';
-  this.name = ''; // Full name, like data-bind or @(whatever.the.expression)?
   this.left = [];
   this.right = [];
   this.rightIsQuoted = false;
