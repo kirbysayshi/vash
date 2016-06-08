11702cb1b76544f60334026fe86b52d6f27111b1
Andrew Petersen <kirbysayshi@gmail.com>
2016-06-08 17:27:03 -0400

    rebuild

--- build/vash-runtime.js
+++ build/vash-runtime.js
@@ -24,7 +24,7 @@ exports.context = function(input, lineno, columnno, linebreak) {
 module.exports={
   "name": "vash",
   "description": "Razor syntax for JS templating",
-  "version": "0.11.3",
+  "version": "0.12.0",
   "author": "Andrew Petersen <senofpeter@gmail.com>",
   "homepage": "https://github.com/kirbysayshi/vash",
   "bin": {
--- build/vash.js
+++ build/vash.js
@@ -3420,7 +3420,7 @@ process.umask = function() { return 0; };
 module.exports={
   "name": "vash",
   "description": "Razor syntax for JS templating",
-  "version": "0.11.3",
+  "version": "0.12.0",
   "author": "Andrew Petersen <senofpeter@gmail.com>",
   "homepage": "https://github.com/kirbysayshi/vash",
   "bin": {
